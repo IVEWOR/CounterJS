51825c90ff7de0a822289f71936ba9fc6c7e09e8
IVEWOR <realblackmario@gmail.com>
2021-05-22 06:54:29 +0530

    Added some styles & Improved the existing to change colors based on minus or plus number

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 let counter = parseInt(document.getElementById("counter").innerHTML);
+
+// Change color
+function changeColor() {
+  const counterString = document.getElementById("counter");
+  if (counter < 0) {
+    counterString.style.color = "red";
+  } else if (counter > 0) {
+    counterString.style.color = "green";
+  } else {
+    counterString.style.color = "#000";
+  }
+}
+
 // Increase function
 document.getElementById("increase").addEventListener("click", function () {
   counter++;
   document.getElementById("counter").innerHTML = counter;
+  changeColor();
 });
 // Decrease function
 document.getElementById("decrease").addEventListener("click", function () {
   counter--;
   document.getElementById("counter").innerHTML = counter;
+  changeColor();
 });
-//Reset zero
+// Reset function
 document.getElementById("reset").addEventListener("click", function () {
   counter = 0;
   document.getElementById("counter").innerHTML = counter;
+  changeColor();
 });
